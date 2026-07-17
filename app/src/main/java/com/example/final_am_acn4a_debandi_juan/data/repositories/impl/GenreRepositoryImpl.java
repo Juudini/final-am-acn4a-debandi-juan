92b01153d074f3f01c2fef0f72785291685efd33
Juani <juudinidev@gmail.com>
2026-07-17 04:21:29 +0200

    feat(data): implement movie and genre repositories

---
+++ app/src/main/java/com/example/final_am_acn4a_debandi_juan/data/repositories/impl/GenreRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.example.final_am_acn4a_debandi_juan.data.repositories.impl;
+
+import androidx.annotation.NonNull;
+
+import com.example.final_am_acn4a_debandi_juan.data.datasources.network.TmdbDatasource;
+import com.example.final_am_acn4a_debandi_juan.data.models.Genre;
+import com.example.final_am_acn4a_debandi_juan.data.models.GenreResponse;
+import com.example.final_am_acn4a_debandi_juan.data.repositories.DataCallback;
+import com.example.final_am_acn4a_debandi_juan.data.repositories.GenreRepository;
+
+import java.util.Collections;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public final class GenreRepositoryImpl implements GenreRepository {
+    private final TmdbDatasource networkDataSource;
+    public GenreRepositoryImpl(TmdbDatasource networkDataSource) {
+        this.networkDataSource = networkDataSource;
+    }
+
+    @Override
+    public void getGenres(DataCallback<List<Genre>> callback) {
+        networkDataSource.getGenres().enqueue(new Callback<GenreResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<GenreResponse> call, @NonNull Response<GenreResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    callback.onSuccess(response.body().getGenres() != null
+                            ? response.body().getGenres()
+                            : Collections.emptyList());
+                } else {
+                    callback.onError("Can't get the genres");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<GenreResponse> call, @NonNull Throwable error) {
+                callback.onError(error.getMessage());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/final_am_acn4a_debandi_juan/data/repositories/impl/MovieRepositoryImpl.java
@@ -0,0 +1,108 @@
+package com.example.final_am_acn4a_debandi_juan.data.repositories.impl;
+
+import androidx.annotation.NonNull;
+import com.example.final_am_acn4a_debandi_juan.data.datasources.network.TmdbDatasource;
+import com.example.final_am_acn4a_debandi_juan.data.models.CastMember;
+import com.example.final_am_acn4a_debandi_juan.data.models.CreditsResponse;
+import com.example.final_am_acn4a_debandi_juan.data.models.Movie;
+import com.example.final_am_acn4a_debandi_juan.data.models.MovieDetail;
+import com.example.final_am_acn4a_debandi_juan.data.models.MovieResponse;
+import com.example.final_am_acn4a_debandi_juan.data.repositories.DataCallback;
+import com.example.final_am_acn4a_debandi_juan.data.repositories.MovieRepository;
+
+import java.util.Collections;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public final class MovieRepositoryImpl implements MovieRepository {
+    private static final String NETWORK_ERROR = "Can't get the data";
+    private final TmdbDatasource networkDataSource;
+
+    public MovieRepositoryImpl(TmdbDatasource networkDataSource) {
+        this.networkDataSource = networkDataSource;
+    }
+
+    @Override
+    public void getTrending(DataCallback<List<Movie>> callback) {
+        networkDataSource.getTrending().enqueue(movieListCallback(callback));
+    }
+
+    @Override
+    public void getNowPlaying(int page, DataCallback<List<Movie>> callback) {
+        networkDataSource.getNowPlaying(page).enqueue(movieListCallback(callback));
+    }
+
+    @Override
+    public void searchMovies(String query, int page, DataCallback<List<Movie>> callback) {
+        networkDataSource.searchMovies(query, page).enqueue(movieListCallback(callback));
+    }
+
+    @Override
+    public void discoverByGenre(int genreId, String sortBy, int page, DataCallback<List<Movie>> callback) {
+        networkDataSource.discoverByGenre(genreId, sortBy, page).enqueue(movieListCallback(callback));
+    }
+
+    @Override
+    public void getMovieDetail(int movieId, DataCallback<MovieDetail> callback) {
+        networkDataSource.getMovieDetail(movieId).enqueue(new Callback<MovieDetail>() {
+            @Override
+            public void onResponse(@NonNull Call<MovieDetail> call, @NonNull Response<MovieDetail> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    callback.onSuccess(response.body());
+                } else {
+                    callback.onError(NETWORK_ERROR);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<MovieDetail> call, @NonNull Throwable error) {
+                callback.onError(error.getMessage());
+            }
+        });
+    }
+
+    @Override
+    public void getMovieCredits(int movieId, DataCallback<List<CastMember>> callback) {
+        networkDataSource.getMovieCredits(movieId).enqueue(new Callback<CreditsResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<CreditsResponse> call, @NonNull Response<CreditsResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    callback.onSuccess(response.body().getCast() != null
+                            ? response.body().getCast()
+                            : Collections.emptyList());
+                } else {
+                    callback.onError(NETWORK_ERROR);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<CreditsResponse> call, @NonNull Throwable error) {
+                callback.onError(error.getMessage());
+            }
+        });
+    }
+
+    private Callback<MovieResponse> movieListCallback(DataCallback<List<Movie>> callback) {
+        return new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<MovieResponse> call, @NonNull Response<MovieResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    callback.onSuccess(response.body().getResults() != null
+                            ? response.body().getResults()
+                            : Collections.emptyList());
+                } else {
+                    callback.onError(NETWORK_ERROR);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<MovieResponse> call, @NonNull Throwable error) {
+                callback.onError(error.getMessage());
+            }
+        };
+    }
+}
